9ee27bf381b10d0fb2576e894f6730cfde10d83b
amikha1lov <mikha1lov@rambler.ru>
2021-12-20 23:02:39 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let swiper = new Swiper(".mySwiper", {
+
+    const pay_modal_run = document.querySelector('.pay_modal_run');
+    const modal_utm = document.querySelector('.modal_utm');
+    const modal_method = document.querySelector('.modal_method');
+    const modal_amount = document.querySelector('.modal_amount');
+    const modal_type = document.querySelector('.modal_type');
+    const main_pay_form = document.querySelector('.main_pay_form');
+    const modal_to_pay = document.querySelector('.modal_to_pay');
+    const modal_pay_form = document.querySelector('.modal_pay_form');
+    const menu_open = document.querySelector('.menu_open');
+    const menu_block = document.querySelector('.menu_block');
+
+
+    menu_open.addEventListener('click', e => {
+        menu_block.classList.toggle('mobile:block');
+    });
+
+    let swiper_about = new Swiper(".slider_about", {
         slidesPerView: 1,
-        direction: 'horizontal',
-        loop: true,
+        spaceBetween: 30,
         pagination: {
             el: ".swiper-pagination",
         },
     });
+
+    let swiper_kids = new Swiper(".slider_kids", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+
+    MicroModal.init({
+        openTrigger: 'data-custom-open',
+        closeTrigger: 'data-custom-close',
+        disableScroll: true,
+        disableFocus: true,
+        awaitOpenAnimation: true,
+        awaitCloseAnimation: true
+    });
+
+    pay_modal_run.addEventListener('click', e => {
+       let params = (new URL(document.location)).searchParams;
+       modal_utm.value = params.get("utm_source");
+       modal_type.value = e.currentTarget.closest('.main_pay_form').querySelector('input[name="pay_type"]:checked').value;
+       modal_method.value = e.currentTarget.closest('.main_pay_form').querySelector('input[name="pay_method"]:checked').value;
+       modal_amount.value = e.currentTarget.closest('.main_pay_form').querySelector('input[name="pay_amount"]:checked').value;
+    });
+
+    modal_to_pay.addEventListener('click', e => {
+       e.preventDefault();
+
+       let data = new FormData(modal_pay_form);
+
+        $.ajax({
+            type: "POST",
+            url: '/local/ajax/pay.php',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: data,
+            dataType : 'json',
+            success: function(msg){
+                if (msg.error == '') {
+                    console.log(msg);
+                } else {
+                    console.log(msg);
+                }
+            }
+        });
+
+
+    });
+
+
+
+
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     './**/*.html',
   ],
   theme: {
+    screens: {
+      'tablet': {'max': '1024px'},
+      'mobile': {'max': '475px'},
+      'mobile-mini': {'max': '320px'},
+    },
     extend: {
       fontFamily: {
         PTSans: "'PT Sans', sans-serif",
@@ -12,11 +17,16 @@ module.exports = {
 
       },
       backgroundImage: {
-        'hero': "url('/img/hero-bg.jpg')",
-        'middle': "url('/img/ng-bg.png')",
-        'card': "url('/img/card-bg.png')",
-        'facts': "url('/img/facts-bg.png')"
-
+        'hero': "url('../img/hero-bg.jpg')",
+        'hero-tablet': "url('../img/hero-bg-tablet.jpg')",
+        'hero-mobile': "url('../img/hero-bg-mobile.png')",
+        'middle': "url('../img/ng-bg.png')",
+        'card': "url('../img/card-bg.png')",
+        'facts': "url('../img/facts-bg.png')",
+        'santa_block': "url('../img/santa_block.png')",
+        'card-mobile': "url('../img/card-bg-mobile.png')",
+        'menu': "url('../img/menu-bg.jpg')",
+        'card_kids_mobile': "url('../img/card_kids_mobile.jpg')",
       }
     },
   },
